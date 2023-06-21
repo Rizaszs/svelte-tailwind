2e90340c7768423cddd62535e9b6e7417a18448b
suryawan <rizalsuryawan86@gmail.com>
2023-06-21 10:37:12 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<section class="min-h-screen w-full bg-gray-200 flex flex-col justify-center items-center">
+	<div class=" bg-white rounded-lg p-5">
+		<h1 class=" text-4xl text-zinc-500">Hello world</h1>
+	</div>
+</section>
